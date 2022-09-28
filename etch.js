906311ff1c95fafaf0613565c41dae1adc451c9e
code_chode <sbelanger1@gmail.com>
2022-09-27 23:56:09 -0400

    added event listeners for on Click event to change div background colors added event listener to reload page when pressing 'clear' button.

--- etch.js
+++ etch.js
@@ -2,20 +2,32 @@ const container = document.createElement('div');
 container.classList.add('container');
 document.body.appendChild(container);
 
+const clear = document.createElement('button');
+clear.classList.add('clear');
+clear.textContent = "CLEAR";
+document.body.appendChild(clear);
 
 function createDivs(num1, num2) {
 
     for ( i = 0; i < num1 * num2; i++ ) {
+
         let squares = document.createElement('div');
-        squares.classList.add('squares')
+        squares.classList.add('squares');
         container.appendChild(squares);
-        
+
+        squares.addEventListener('mousedown', (e) => {
+            squares.style.backgroundColor = 'black';
+        });
     }
+}
 
-    squares.addEventListener('click', (e) => {
-        e.style.backgroundColor = 'black';
-    });
+function clickSquare() {
 
+    squares.style.backgroundColor = 'black';
 }
 
 createDivs(16, 16);
+
+clear.addEventListener('click', function() {
+    location.reload();
+});
